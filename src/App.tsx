703748f69ef20252e920e0c2295ccc1416bc0acd
Maksim Šetić <seticmaksim@gmail.com>
2026-03-20 11:01:23 +0100

    #19 - Added aircash link

--- src/App.tsx
+++ src/App.tsx
@@ -860,18 +860,20 @@ function App() {
                   Ko-fi
                 </a>
                 <a
-                  href={socialLinks.email}
-                  className="support-link email"
-                  title="E-mail"
+                  href="https://m3.aircash.eu/acpay/martins3764"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="support-link aircash"
+                  title="Aircash"
                 >
                   <svg
                     viewBox="0 0 24 24"
                     fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg"
                   >
-                    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z" />
+                    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4H4v2h16V8zm0 4H4v2h16v-2zm0 4H4v2h16v-2z" />
                   </svg>
-                  E-mail
+                  Aircash
                 </a>
                 <a
                   href={socialLinks.youtube}
